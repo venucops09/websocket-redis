c02dd9a720d20ef37416e47f1405a838746d24e3
venucops009 <venugopal.chinnadurai@e5.ai>
2025-05-27 19:03:13 +0530

    Moved to new directory

--- src/main/java/com/websocket/redis/messaging/server/dto/ConnectionState.java
+++
@@ -1,7 +0,0 @@
-package com.websocket.redis.messaging.server.dto;
-
-public enum ConnectionState {
-    CONNECTED,
-    DISCONNECTED
-}
-
---
+++ websocket-redis/src/main/java/com/websocket/redis/messaging/server/dto/MessageType.java
@@ -0,0 +1,7 @@
+package com.websocket.redis.messaging.server.dto;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
